cee7fb3c452ae0c17e7952bd589ca7fceba546e1
Max <teratoscincus@hotmail.com>
2024-03-31 15:32:13 +0200

    feat(args): print help if no args given

--- milestone_tracker.py
+++ milestone_tracker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from cli import tui
-from cli.args import args
+from cli.args import args, parser
 from milestones.milestone import Milestone
 from milestones.storage import Storage
 
@@ -12,11 +12,14 @@ if args.add:
 
     STORAGE.add_milestone(Milestone(**details))
 
-if args.list_milestones:
+elif args.list_milestones:
     tui.list_milestones(STORAGE.milestones)
 
-if args.time_since:
+elif args.time_since:
     event = args.time_since
     date_details = STORAGE.milestones[event]
 
     tui.print_time_passed_since(Milestone(event, **date_details))
+
+else:
+    parser.print_help()
